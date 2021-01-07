933ce05be68049f5671f347778e73c90b6cf350f
Jefte <jefte199@gmail.com>
2021-01-07 10:26:51 -0300

    fix: server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,19 @@
 import './util/module-alias';
 import { Server } from '@overnightjs/core';
+import bodyParser from 'body-parser';
 
-export class SetupServer extends Server {}
+export class SetupServer extends Server {
+
+  constructor(private port = 3000){
+    super();
+  }
+
+  public init(): void {
+    this.setupExpress();
+  }
+
+  private setupExpress(): void {
+    this.app.use(bodyParser.json());
+  }
+}
 
